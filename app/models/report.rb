5f14bbfe9396438eec302ba6db641bc2c7b4d9f2
reizist <reizist@gmail.com>
2017-04-20 03:13:48 +0900

    Using serialize for treating array column. mysql can't define array column by default

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,6 +4,8 @@ class Report < ApplicationRecord
   belongs_to :account
   belongs_to :target_account, class_name: 'Account'
   belongs_to :action_taken_by_account, class_name: 'Account'
+  
+  serialize :status_ids, Array
 
   scope :unresolved, -> { where(action_taken: false) }
   scope :resolved,   -> { where(action_taken: true) }
