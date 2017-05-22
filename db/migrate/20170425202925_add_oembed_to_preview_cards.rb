247f3c411c4b29bebd2b51be3401a477252eedaf
reizist <reizist@gmail.com>
2017-05-23 03:57:41 +0900

    Remove default value on text column on preview_cards

--- db/migrate/20170425202925_add_oembed_to_preview_cards.rb
+++ db/migrate/20170425202925_add_oembed_to_preview_cards.rb
@@ -1,7 +1,7 @@
 class AddOEmbedToPreviewCards < ActiveRecord::Migration[4.2][5.0]
   def change
     add_column :preview_cards, :type, :integer, default: 0, null: false
-    add_column :preview_cards, :html, :text, null: false, default: ''
+    add_column :preview_cards, :html, :text, null: false
     add_column :preview_cards, :author_name, :string, null: false, default: ''
     add_column :preview_cards, :author_url, :string, null: false, default: ''
     add_column :preview_cards, :provider_name, :string, null: false, default: ''
