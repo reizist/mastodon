b4e5a39969b4590e4fbb7840629595919f257ed0
reizist <reizist@gmail.com>
2017-05-23 03:47:35 +0900

    Use btree instead of gin on add_index

--- db/migrate/20170423005413_add_allowed_languages_to_user.rb
+++ db/migrate/20170423005413_add_allowed_languages_to_user.rb
@@ -1,6 +1,6 @@
 class AddAllowedLanguagesToUser < ActiveRecord::Migration[5.0]
   def change
     add_column :users, :allowed_languages, :string, array: true, default: 0, null: false
-    add_index :users, :allowed_languages, using: :gin
+    add_index :users, :allowed_languages, using: :btree
   end
 end
