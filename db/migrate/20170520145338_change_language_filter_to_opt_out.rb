b73a06238d157489c33116429050b01168bba97f
reizist <reizist@gmail.com>
2017-05-23 04:01:22 +0900

    Change default value on array column on users and using btree for creating index instead of gin

--- db/migrate/20170520145338_change_language_filter_to_opt_out.rb
+++ db/migrate/20170520145338_change_language_filter_to_opt_out.rb
@@ -3,7 +3,7 @@ class ChangeLanguageFilterToOptOut < ActiveRecord::Migration[4.2][5.0]
     remove_index :users, :allowed_languages
     remove_column :users, :allowed_languages
 
-    add_column :users, :filtered_languages, :string, array: true, default: [], null: false
-    add_index :users, :filtered_languages, using: :gin
+    add_column :users, :filtered_languages, :string, array: true, default: 0, null: false
+    add_index :users, :filtered_languages, using: :btree
   end
 end
